0fdc3f5a59f32a750a6db221c00c143bc0af195e
Dracarys <laurine.sorel@free.fr>
2017-08-04 13:38:21 +0800

    [i3 / Polybar / Rofi] Add configuration

--- polybar/pkg.sh
+++ polybar/pkg.sh
@@ -1,1 +1,21 @@
-/home/dracarys/.config/polybar/pkg.sh
+#!/bin/bash
+pac=$(checkupdates | wc -l)
+aur=$(cower -u | wc -l)
+
+check=$((pac + aur))
+if [[ "$check" != "0" ]]
+then
+    if [[ "$pac" > "100" ]]
+    then
+        PAC='%{F#AFF9F5}'
+    else
+        PAC='%{F#ffffff}'
+    fi
+    if [[ "$aur" > "10" ]]
+    then
+        AUR='%{F#AFF9F5}'
+    else
+        AUR='%{F#ffffff}'
+    fi
+    echo "${PAC} $pac %{F+} %{F#ffffff}%{F+} ${AUR} $aur"
+fi
--- polybar/test.sh
+++ polybar/test.sh
@@ -1,1 +1,8 @@
-/home/dracarys/.config/polybar/test.sh
+#!/bin/sh
+i3status | while :
+do
+    read line
+    echo "TODO:$(task ls | sed -n '4s/[[:blank:]]\+/ /pg' )\
+| LG: $(setxkbmap -print | grep xkb_symbols | awk -F"+" '{print $2}') \
+| $line" || exit 1
+done
